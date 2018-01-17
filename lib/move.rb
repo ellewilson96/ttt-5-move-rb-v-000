55a8a2e1ae0fb3cd8c8e9d8603c7c4aae5fc7f24
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-17 20:50:33 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,16 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index("1"-"9")
+  "1".to_i-1
+  "2".to_i-1
+  "3".to_i-1
+  "4".to_i-1
+  "5".to_i-1
+  "6".to_i-1
+  "7".to_i-1
+  "8".to_i-1
+  "9".to_i-1
+end
+
+  
