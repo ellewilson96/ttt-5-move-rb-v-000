6fd35a240eecf09c7a8a22c1549c1beecfda68c3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-17 21:01:26 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,10 +11,10 @@ def input_to_index(numbers)
   ("1".to_i) - 1
   ("2".to_i) - 1
   ("3".to_i) - 1
-  "4".to_i - 1
-  "5".to_i - 1
-  "6".to_i - 1
-  "7".to_i - 1
-  "8".to_i - 1
-  "9".to_i - 1
+  ("4".to_i) - 1
+  ("5".to_i) - 1
+  ("6".to_i) - 1
+  ("7".to_i) - 1
+  ("8".to_i) - 1
+  ("9".to_i) - 1
 end
